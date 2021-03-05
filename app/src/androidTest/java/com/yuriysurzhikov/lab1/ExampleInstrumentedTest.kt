e4dde82c4bc28d0a99393c38058c968fbd3c234c
yuriysurzhikov <surzhickov.yura@yandex.ua>
2021-03-06 00:09:27 +0200

    Created simple test

--- app/src/androidTest/java/com/yuriysurzhikov/lab1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/yuriysurzhikov/lab1/ExampleInstrumentedTest.kt
@@ -1,24 +1,37 @@
 package com.yuriysurzhikov.lab1
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.hamcrest.CoreMatchers.allOf
 
 import org.junit.Test
 import org.junit.runner.RunWith
 
-import org.junit.Assert.*
+import org.junit.Rule
 
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+
+    private val name1 = "Yuriy"
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
     @Test
     fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.yuriysurzhikov.lab1", appContext.packageName)
+        onView(withId(R.id.text_input))
+            .check(matches(allOf(isDisplayed(), isEnabled())))
+            .perform(typeText(name1), pressImeActionButton())
+        val resultText = String.format(
+            InstrumentationRegistry.getInstrumentation().targetContext.getString(R.string.greetings_template),
+            name1
+        )
+        onView(withId(R.id.greetings_text))
+            .check(matches(withText(resultText)))
     }
 }
